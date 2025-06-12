87be408bc040de21a37f99e2b67dc90cd7bdaf90
KFalcao <karollinefalcao@gmail.com>
2025-06-12 00:12:45 -0300

    :bricks: Feat:Estrutura de pastas e configurações iniciais

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+export default function App() {
+  return(
+   <main>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <p className="text-lg text-gray-700">
+      This is a simple React app with Tailwind CSS.
+    </p>
+   </main>
+  )
+}
